c8a96828aee2194093ddca1625da90fbc5479379
Putra <adisps9955@gmail.com>
2025-04-12 17:27:06 +0700

    🛠️ feat(ResponseMapper): add context param to map method

--- archtype/src/main/java/io/github/adiitgg/arch/platform/misc/ResponseMapper.java
+++ archtype/src/main/java/io/github/adiitgg/arch/platform/misc/ResponseMapper.java
@@ -1,15 +1,16 @@
 package io.github.adiitgg.arch.platform.misc;
 
-import io.github.adiitgg.arch.platform.model.BaseResponse;
 import io.avaje.inject.Component;
+import io.github.adiitgg.arch.platform.model.BaseResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.RoutingContext;
 
 @Component
 public class ResponseMapper implements io.github.adiitgg.vertx.http.response.ResponseMapper {
 
   @Override
-  public Buffer map(Object result) {
+  public Buffer map(RoutingContext context, Object result) {
     if (result instanceof Buffer buffer) {
       return buffer;
     }
