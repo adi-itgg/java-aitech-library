b8b434367b758db7dff5d2b5e3d902c5190422f1
adi <syahdilla.ps@gmail.com>
2025-07-27 20:08:51 +0700

    ✅ test(ConfigRetrieverExtended): Add tests for null, empty, and short arguments

--- vertx-config/src/test/java/io/github/adiitgg/vertx/config/ConfigRetrieverExtendedTest.java
+++ vertx-config/src/test/java/io/github/adiitgg/vertx/config/ConfigRetrieverExtendedTest.java
@@ -30,4 +30,31 @@ public class ConfigRetrieverExtendedTest {
     assert config.getInteger("app.port").equals(8080);
   }
 
+  @Test
+  void shortArgsProfileNotExists(Vertx vertx) {
+    val configurationRetrieverExtended = ConfigRetrieverExtended.create(vertx)
+      .args(new String[]{"-p=production"})
+      .build();
+    val config = YmlJsonObject.of(configurationRetrieverExtended.getConfig().toCompletionStage().toCompletableFuture().join());
+    assert config.getInteger("app.port").equals(8080);
+  }
+
+  @Test
+  void noArgs(Vertx vertx) {
+    val configurationRetrieverExtended = ConfigRetrieverExtended.create(vertx)
+      .args(null)
+      .build();
+    val config = YmlJsonObject.of(configurationRetrieverExtended.getConfig().toCompletionStage().toCompletableFuture().join());
+    assert config.getInteger("app.port").equals(8080);
+  }
+
+  @Test
+  void emptyArgs(Vertx vertx) {
+    val configurationRetrieverExtended = ConfigRetrieverExtended.create(vertx)
+      .args(new String[0])
+      .build();
+    val config = YmlJsonObject.of(configurationRetrieverExtended.getConfig().toCompletionStage().toCompletableFuture().join());
+    assert config.getInteger("app.port").equals(8080);
+  }
+
 }
