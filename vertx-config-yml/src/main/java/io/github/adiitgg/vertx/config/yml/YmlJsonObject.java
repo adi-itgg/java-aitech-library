5520fd04d4ade89887c4a25f8820e893ac7552ee
adi <syahdilla.ps@gmail.com>
2025-07-27 20:37:20 +0700

    ✨ feat(YmlJsonObject): Add `of` factory methods with extensive test coverage

--- vertx-config-yml/src/main/java/io/github/adiitgg/vertx/config/yml/YmlJsonObject.java
+++ vertx-config-yml/src/main/java/io/github/adiitgg/vertx/config/yml/YmlJsonObject.java
@@ -76,6 +76,305 @@ public class YmlJsonObject extends JsonObject {
     return new YmlJsonObject();
   }
 
+
+  /**
+   * Create a YmlJsonObject containing a single mapping.
+   *
+   * @param k1 the mapping's key
+   * @param v1 the mapping's value
+   * @return a YmlJsonObject containing the specified mapping.
+   */
+  public static YmlJsonObject of(String k1, Object v1) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(1));
+
+    obj.put(k1, v1);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing two mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(2));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing three mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(3));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing four mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(4));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing five mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(5));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing six mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @param k6 the sixth mapping's key
+   * @param v6 the sixth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5, String k6, Object v6) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(6));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+    obj.put(k6, v6);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing seven mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @param k6 the sixth mapping's key
+   * @param v6 the sixth mapping's value
+   * @param k7 the seventh mapping's key
+   * @param v7 the seventh mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5, String k6, Object v6,
+                              String k7, Object v7) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(7));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+    obj.put(k6, v6);
+    obj.put(k7, v7);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing eight mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @param k6 the sixth mapping's key
+   * @param v6 the sixth mapping's value
+   * @param k7 the seventh mapping's key
+   * @param v7 the seventh mapping's value
+   * @param k8 the eighth mapping's key
+   * @param v8 the eighth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5, String k6, Object v6,
+                              String k7, Object v7, String k8, Object v8) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(8));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+    obj.put(k6, v6);
+    obj.put(k7, v7);
+    obj.put(k8, v8);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing nine mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @param k6 the sixth mapping's key
+   * @param v6 the sixth mapping's value
+   * @param k7 the seventh mapping's key
+   * @param v7 the seventh mapping's value
+   * @param k8 the eighth mapping's key
+   * @param v8 the eighth mapping's value
+   * @param k9 the ninth mapping's key
+   * @param v9 the ninth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5, String k6, Object v6,
+                              String k7, Object v7, String k8, Object v8, String k9, Object v9) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(9));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+    obj.put(k6, v6);
+    obj.put(k7, v7);
+    obj.put(k8, v8);
+    obj.put(k9, v9);
+
+    return obj;
+  }
+
+  /**
+   * Create a YmlJsonObject containing ten mappings.
+   *
+   * @param k1 the first mapping's key
+   * @param v1 the first mapping's value
+   * @param k2 the second mapping's key
+   * @param v2 the second mapping's value
+   * @param k3 the third mapping's key
+   * @param v3 the third mapping's value
+   * @param k4 the fourth mapping's key
+   * @param v4 the fourth mapping's value
+   * @param k5 the fifth mapping's key
+   * @param v5 the fifth mapping's value
+   * @param k6 the sixth mapping's key
+   * @param v6 the sixth mapping's value
+   * @param k7 the seventh mapping's key
+   * @param v7 the seventh mapping's value
+   * @param k8 the eighth mapping's key
+   * @param v8 the eighth mapping's value
+   * @param k9 the ninth mapping's key
+   * @param v9 the ninth mapping's value
+   * @param k10 the tenth mapping's key
+   * @param v10 the tenth mapping's value
+   * @return a YmlJsonObject containing the specified mappings.
+   */
+  public static YmlJsonObject of(String k1, Object v1, String k2, Object v2, String k3, Object v3,
+                              String k4, Object v4, String k5, Object v5, String k6, Object v6,
+                              String k7, Object v7, String k8, Object v8, String k9, Object v9,
+                              String k10, Object v10) {
+    YmlJsonObject obj = new YmlJsonObject(new LinkedHashMap<>(10));
+
+    obj.put(k1, v1);
+    obj.put(k2, v2);
+    obj.put(k3, v3);
+    obj.put(k4, v4);
+    obj.put(k5, v5);
+    obj.put(k6, v6);
+    obj.put(k7, v7);
+    obj.put(k8, v8);
+    obj.put(k9, v9);
+    obj.put(k10, v10);
+
+    return obj;
+  }
+
+
   /**
    * Create a YmlJsonObject containing a single mapping.
    *
@@ -205,7 +504,7 @@ public class YmlJsonObject extends JsonObject {
     return YmlJsonObject.of(super.getJsonObject(key));
   }
 
-  public YmlJsonObject getYmlJsonObject(String key, JsonObject def) {
+  public YmlJsonObject getYmlJsonObject(String key, YmlJsonObject def) {
     return YmlJsonObject.of(super.getJsonObject(key, def));
   }
 
--- vertx-config-yml/src/test/java/io/github/adiitgg/vertx/config/yml/YmlJsonObjectTest.java
+++ vertx-config-yml/src/test/java/io/github/adiitgg/vertx/config/yml/YmlJsonObjectTest.java
@@ -1,14 +1,18 @@
 package io.github.adiitgg.vertx.config.yml;
 
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class YmlJsonObjectTest {
 
   @Test
   void put() {
-    // Test the put method with different types of values
     YmlJsonObject jsonObject = new YmlJsonObject();
     jsonObject.put("key1", "value1");
     jsonObject.put("user.name", "testing");
@@ -18,16 +22,234 @@ class YmlJsonObjectTest {
   }
 
   @Test
+  void putNull() {
+    YmlJsonObject jsonObject = new YmlJsonObject();
+    jsonObject.putNull("key1");
+    jsonObject.putNull("user.name");
+    // Assertions
+    assertNull(jsonObject.getString("key1"));
+    assertNull(jsonObject.getString("user.name"));
+  }
+
+  @Test
   void remove() {
-      // Test the remove method with different types of values
-      YmlJsonObject jsonObject = new YmlJsonObject();
-      jsonObject.put("key1", "value1");
-      jsonObject.put("user.name", "testing");
-      // Assertions
-      assertEquals("value1", jsonObject.remove("key1"));
-      assertEquals(null, jsonObject.getValue("key1"));
-      assertEquals("testing", jsonObject.remove("user.name"));
-      assertEquals(null, jsonObject.getString("user.name"));
+    YmlJsonObject jsonObject = new YmlJsonObject();
+    jsonObject.put("key1", "value1");
+    jsonObject.put("user.name", "testing");
+    // Assertions
+    assertEquals("value1", jsonObject.remove("key1"));
+    assertNull(jsonObject.getValue("key1"));
+    assertEquals("testing", jsonObject.remove("user.name"));
+    assertNull(jsonObject.getString("user.name"));
+  }
+
+  @Test
+  void jsonObjectToYmlJsonObject() {
+    JsonObject jsonObject = JsonObject.of("key1", "value1", "user.name", "testing");
+    JsonObject nestedJsonObject = JsonObject.of("key2", "value2");
+    jsonObject.put("nested", nestedJsonObject);
+    YmlJsonObject ymlJsonObject = YmlJsonObject.of(jsonObject);
+    // Assertions
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+  }
+
+  @Test
+  void jsonFromStringToYmlJsonObject() {
+    String json = "{\"key1\": \"value1\", \"user.name\": \"testing\"}";
+    YmlJsonObject ymlJsonObject = new YmlJsonObject(json);
+    // Assertions
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+  }
+
+  @Test
+  void jsonFromBufferToYmlJsonObject() {
+    Buffer json = Buffer.buffer("{\"key1\": \"value1\", \"user.name\": \"testing\"}");
+    YmlJsonObject ymlJsonObject = new YmlJsonObject(json);
+    // Assertions
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+  }
+
+  @Test
+  void ymlJsonObjectOfArgs() {
+    YmlJsonObject ymlJsonObject = YmlJsonObject.of("key1", "value1");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+    assertEquals("value7", ymlJsonObject.getString("key7"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7", "key8", "value8");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+    assertEquals("value7", ymlJsonObject.getString("key7"));
+    assertEquals("value8", ymlJsonObject.getString("key8"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7", "key8", "value8", "key9", "value9");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+    assertEquals("value7", ymlJsonObject.getString("key7"));
+    assertEquals("value8", ymlJsonObject.getString("key8"));
+    assertEquals("value9", ymlJsonObject.getString("key9"));
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "user.name", "testing", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7", "key8", "value8", "key9", "value9", "key10", "value10");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("testing", ymlJsonObject.getString("user.name"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+    assertEquals("value7", ymlJsonObject.getString("key7"));
+    assertEquals("value8", ymlJsonObject.getString("key8"));
+    assertEquals("value9", ymlJsonObject.getString("key9"));
+    assertEquals("value10", ymlJsonObject.getString("key10"));
+
+
+    ymlJsonObject = YmlJsonObject.of("key1", "value1", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7", "key8", "value8", "key9", "value9", "key10", "value10", "key11", "value11");
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+    assertEquals("value3", ymlJsonObject.getString("key3"));
+    assertEquals("value4", ymlJsonObject.getString("key4"));
+    assertEquals("value5", ymlJsonObject.getString("key5"));
+    assertEquals("value6", ymlJsonObject.getString("key6"));
+    assertEquals("value7", ymlJsonObject.getString("key7"));
+    assertEquals("value8", ymlJsonObject.getString("key8"));
+    assertEquals("value9", ymlJsonObject.getString("key9"));
+    assertEquals("value10", ymlJsonObject.getString("key10"));
+    assertEquals("value11", ymlJsonObject.getString("key11"));
+  }
+
+  @Test
+  void ymlJsonObjectInvalidOfArgs() {
+    try {
+      YmlJsonObject.of("key1", "value1", "key2", "value2", "key3", "value3", "key4", "value4", "key5", "value5", "key6", "value6", "key7", "value7", "key8", "value8", "key9", "value9", "key10", "value10", "key11");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Invalid number of arguments", e.getMessage());
+    }
+  }
+
+  @Test
+  void mapFrom_null() {
+    assertNull(YmlJsonObject.mapFrom(null));
+  }
+
+  @Test
+  void mapFrom_success() {
+    Map<String, Object> map = new LinkedHashMap<>();
+    map.put("key1", "value1");
+    YmlJsonObject ymlJsonObject = YmlJsonObject.mapFrom(map);
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+  }
+
+  @Test
+  void getJsonObjectDefault() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    YmlJsonObject jsonObject = ymlJsonObject.getJsonObject("key1", YmlJsonObject.of());
+    assertNotNull(jsonObject);
+  }
+
+  @Test
+  void mergeIn_success() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    ymlJsonObject.mergeIn(YmlJsonObject.of("key1", "value1"));
+    ymlJsonObject.mergeIn(YmlJsonObject.of("key2", "value2"));
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+    assertEquals("value2", ymlJsonObject.getString("key2"));
+  }
+
+  @Test
+  void mergeIn_deep_success() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    ymlJsonObject.mergeIn(YmlJsonObject.of("key1", "value1"), true);
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+  }
+
+  @Test
+  void mergeIn_depth1_success() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    ymlJsonObject.mergeIn(YmlJsonObject.of("key1", "value1"), 1);
+    assertEquals("value1", ymlJsonObject.getString("key1"));
+  }
+
+  @Test
+  void copy_success() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    ymlJsonObject.mergeIn(YmlJsonObject.of("key1", "value1"), 1);
+    YmlJsonObject ymlJsonObjectCopy = ymlJsonObject.copy();
+    assertEquals("value1", ymlJsonObjectCopy.getString("key1"));
+  }
+
+  @Test
+  void getYmlJsonObject() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    ymlJsonObject.put("key1", YmlJsonObject.of("key1", "value1"));
+    YmlJsonObject ymlJsonObjectCopy = ymlJsonObject.getYmlJsonObject("key1");
+    assertEquals("value1", ymlJsonObjectCopy.getString("key1"));
+  }
+
+  @Test
+  void getYmlJsonObjectDefault() {
+    YmlJsonObject ymlJsonObject = new YmlJsonObject();
+    assertNotNull(ymlJsonObject.getYmlJsonObject("key1", YmlJsonObject.of()));
   }
 
 }
